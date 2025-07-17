d4924c289f3913aa86fa87f30300f6b2c5b2a664
bruno-po <bruno.lahunmen@gmail.com>
2025-07-16 23:26:20 -0300

    Test ruff com erro de formatação nos imports

---
+++ dummy_model/data/ingestion.py
@@ -0,0 +1,82 @@
+from typing import Dict
+
+import duckdb
+from hydra_zen import zen
+import kagglehub
+from hydra_zen.third_party.pydantic import pydantic_parser
+import pydantic
+from pydantic import BaseModel
+
+from dummy_model.config import PROJ_ROOT
+
+
+class DataIngestion(pydantic.BaseModel):
+
+    def get_data_from_kaggle(kaggle_dataset: str, path: str = None):
+        return kagglehub.dataset_download(kaggle_dataset, path=path)
+
+    def write_raw_data(input_path: str, output_table_name: str = None):
+        return duckdb.sql(
+            f"""
+            SELECT *
+            FROM '{input_path}'
+            """
+        )
+
+
+class MyConfig(BaseModel):
+    from_kaggle: Dict[str, str]
+
+
+@zen(instantiation_wrapper=pydantic_parser)
+def my_app(data: MyConfig):
+    input_path = DataIngestion.get_data_from_kaggle(
+        data.from_kaggle["dataset"],
+        data.from_kaggle["file_name"] 
+    )
+    print(DataIngestion.write_raw_data(input_path))
+
+
+if __name__ == "__main__":
+    my_app.hydra_main(
+        version_base=None,
+        config_path=f"{PROJ_ROOT}/conf",
+        config_name="config"
+    )
+
+
+
+
+# class DataIngestion(pydantic.BaseModel):
+
+#     def get_data_from_kaggle(kaggle_dataset: str, path: str = None):
+#         return kagglehub.dataset_download(kaggle_dataset, path=path)
+
+#     def write_raw_data(input_path: str, output_table_name: str = None):
+#         return duckdb.sql(
+#             f"""
+#             SELECT *
+#             FROM '{input_path}'
+#             """
+#         )
+# 
+# class MyConfig(pydantic.BaseModel):
+#     data: Dict[str, Dict[str, str]]
+#     # from_kaggle: List[Dict[str, str]]
+
+
+
+# @hydra.main(version_base=None, config_path="../../conf", config_name="config")
+# def main(cfg: omegaconf.DictConfig):
+#     validated_config = MyConfig(**cfg)
+#     print(cfg)
+
+#     # input_path = DataIngestion.get_data_from_kaggle(
+#     #     validated_config.from_kaggle["dataset"],
+#     #     validated_config.from_kaggle["file_name"] 
+#     # )
+#     # print(DataIngestion.write_raw_data(input_path))
+    
+
+# if __name__ == "__main__":
+#     main()
